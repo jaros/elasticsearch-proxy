44d817e5b3cc5404bcd633db3b2f4c56f5142bec
jaros <judin.jaroslav@gmail.com>
2016-02-05 22:03:07 +0100

    return Local Host ip address

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,19 +1,24 @@
 package controllers
 
+import java.net.InetAddress
+
+import play.Play
 import play.api.libs.json._
 import play.api.mvc._
+
+import scala.concurrent.Future
 
 class Application extends Controller {
 
 
-
-  def index = Action { request =>
-    val appToken = play.Play.application.configuration.getString("play.crypto.secret")
-
-    Ok(Json.obj(
-      "status" -> "OK",
-      "remote_ip_address" -> request.remoteAddress,
-      "token" -> appToken))
+  def index = Action.async {
+    Future.successful {
+      val appToken = Play.application.configuration.getString("play.crypto.secret")
+      Ok(Json.obj(
+        "status" -> "OK",
+        "remote_ip_address" -> InetAddress.getLocalHost.getHostAddress,
+        "token" -> appToken))
+    }
   }
 
 }
